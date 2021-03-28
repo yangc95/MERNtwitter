b7b61a74a7175978a23097a204d1b027646721a1
Christine Yang <yang.christine.95@gmail.com>
2021-03-28 19:25:35 -0400

    Backend completed

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
-const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
-const db = require('./config/keys').mongoURI;
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
+const passport = require('passport')
+
 mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
-app.get("/", (req, res) => res.send("Hello there Matthew"));
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+// app.get("/", (req, res) => {
+//     const user = new User({
+//         handle: "Jim",
+//         email: "jim@jim.jim",
+//         password: "jimisgreat123"
+//     })
+//     user.save()
+//     res.send("Hello a/A")
+// });
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.listen(port, () => {console.log(`Listening on port ${port}`)});
---
+++ routes/api/users.js
@@ -0,0 +1,110 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const keys = require("../../config/keys");
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+const User = require('../../models/User');
+
+// router.get('/test', (req, res) => {
+//     res.json({ msg: "This is the user route" })
+// });
+
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+    res.json({
+      id: req.user.id,
+      handle: req.user.handle,
+      email: req.user.email
+    });
+  })
+
+router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                errors.email = 'Email already exists';
+                return res.status(400).json(errors)
+            } else {
+                const newUser = new User({
+                    handle: req.body.handle,
+                    email: req.body.email,
+                    password: req.body.password
+                })
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then((user) => res.json(user))
+                            .catch(err => console.log(err))
+                    })
+                })
+
+                // newUser.save().then(user => res.send(user)).catch(err => res.send(err));
+            }
+        })
+});
+
+router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    // const handle = req.body.handle;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                errors.email = 'User not found';
+                return res.status(404).json(errors);
+                // return res.status(404).json({email: "This user does not exist."})
+            } 
+            // else if (user.handle !== handle) {
+            //     return res.status(404).json({handle: "Handle is incorrect."})
+            // }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        // res.json({msg: 'Success'});
+
+                        const payload = {
+                            id: user.id,
+                            handle: user.handle,
+                            email: user.email
+                        }
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: "Bearer " + token
+                                });
+                            }
+                        )
+                        // return res.json({msg: "Success!"});
+                    } else {
+                        errors.password = 'Incorrect password'
+                        return res.status(400).json(errors);
+                        // return res.status(400).json({password: "Incorrect Password"});
+                    }
+                })
+        })
+});
+
+module.exports = router;
